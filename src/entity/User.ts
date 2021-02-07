3f8ff9fb638e6437d1d8c9278fffecd185d338b9
ayman.wkh <ayman.wkh@gmail.com>
2021-02-07 06:42:26 +0200

    remove ts-node

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,28 +1,18 @@
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, Index, CreateDateColumn, UpdateDateColumn, BeforeInsert} from "typeorm";
-import bcrypt from 'bcrypt'
-import { classToPlain, Exclude } from 'class-transformer'
-import { IsEmail, Length } from "class-validator";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, CreateDateColumn, UpdateDateColumn } from "typeorm";
 
 @Entity('users')
 export class User extends BaseEntity{
 
-	@Exclude()	
 	@PrimaryGeneratedColumn()
 	id: number
 
-	@Index()
-	@IsEmail()
 	@Column({unique: true})
 	email: string
 
-	@Index()
-	@Length(3, 100, {message: 'User Name must be at least 3 characters'})
 	@Column({unique: true})
 	username: string
 
-	@Exclude()
 	@Column()
-	@Length(3,100)
 	password: string
 
 	@CreateDateColumn()
@@ -30,13 +20,4 @@ export class User extends BaseEntity{
 
 	@UpdateDateColumn()
 	updatedAt: Date
-
-	@BeforeInsert()
-	async hashPassword() {
-		this.password = await bcrypt.hash(this.password, 6)
-	}
-
-	toJSON() {
-		return classToPlain(this)
-	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,13 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import { createConnection } from "typeorm";
 import express from 'express'
-import morgan from 'morgan'
-import authRouter from './routes/auth'
-import trim from "./middleware/trim";
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
 
-dotenv.config()
 const app = express()
 app.use(express.json())
-app.use(morgan('dev'))
-app.use(trim)
-app.use(cookieParser())
-app.use('/api/auth', authRouter)
 
-app.get('/', (req, res) => res.send('hello world'))
+app.get('/', (_, res) => res.send('hello world'))
 app.listen(5000, async () => {
-	console.log('server running on http://localhost:5000')
+	console.log(`server running on http://localhost:5000`)
 	try {
 		await createConnection()
 		console.log('database connected')
@@ -27,20 +16,3 @@ app.listen(5000, async () => {
 	}
 })
 
-/*createConnection().then(async connection => {
-
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
-
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
-
-    console.log("Here you can setup and run express/koa/any other framework.");
-
-}).catch(error => console.log(error));*/
